fbf791a57d264dd78b0f8b16ada934b4616a0c9c
Arthur Patrakhaev <redred05@mail.ru>
2022-09-19 00:55:02 +0300

    initial commit

---
+++ 41/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int[] InputArrayElements()
+{
+    Console.WriteLine("Введи элементы массива через пробел");
+    string elements = Console.ReadLine()!;
+    string[] elementsSplited = elements.Split(' ');
+    int[] array = new int[elementsSplited.Length];
+
+    for (int i = 0; i < elementsSplited.Length; i++)
+    {
+        array[i] = int.Parse(elementsSplited[i]);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count += 1;
+        }
+    }
+    Console.Write($"Колличество чисел > 0 в массиве = {count}");
+}
+
+PrintArray(InputArrayElements());
---
+++ 43/Program.cs
@@ -0,0 +1,36 @@
+﻿//Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+//y = k1 * x + b1
+//y = k2 * x + b2
+//значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.WriteLine("введи k1");
+double k1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("введи b1");
+double b1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("введи k2");
+double k2 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("введи b2");
+double b2 = double.Parse(Console.ReadLine()!);
+
+double x = 0;
+double y = 0;
+
+result(k1, b1, k2, b2);
+
+void result(double k1, double b1, double k2, double b2)
+{
+    if (k1 == k2 && b1 == b2)
+    {
+        Console.WriteLine("Прямые параллельны");
+    }
+    else if (k1 == k2)
+    {
+        Console.WriteLine("Прямые совпадают");
+    }
+    else
+    {
+        x = (b2 - b1) / (k1 - k2);
+        y = k1 * x + b1;
+        Console.WriteLine($"x = {x}, y = {y}");
+    }
+}
